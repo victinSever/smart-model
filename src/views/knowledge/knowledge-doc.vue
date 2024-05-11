2606e7299896717596262f5d395e33af9c0c5195
钟国政 <victinzhong@163.com>
2024-05-11 18:35:57 +0800

    ✨ feat: 知识文档文件上传

--- src/views/knowledge/knowledge-doc.vue
+++ src/views/knowledge/knowledge-doc.vue
@@ -2,7 +2,8 @@
 import baseService from '@/service/baseService';
 import { computed, onMounted, ref } from 'vue';
 import moment from "moment";
-import { ElMessage } from 'element-plus';
+import { ElMessage, UploadProps } from 'element-plus';
+import { getToken } from '@/utils/cache';
 
 interface IBotParam {
   limit: number;
@@ -44,14 +45,21 @@ const queryParam = ref({
   documentName: '',
   type: '1',
   content: '',
-  charCount: 100
+  charCount: 100,
+  filePath: ''
 });
 const group = ref('0');
-const fillAddContent = computed(() => queryParam.value.documentName && queryParam.value.content);
+const fillAddContent = computed(() =>
+  curType.value === 'pdf' ? queryParam.value.documentName && queryParam.value.filePath :
+    queryParam.value.documentName && queryParam.value.content
+);
 const isToken = ref(true);
 const isType = ref(false);
 const splitList = ref<any>([]);
 const saveLoading = ref(false);
+const headers = ref({ token: getToken() });
+const action = ref('https://53r23232p6.goho.co/api/file/upload');
+
 
 onMounted(() => {
   getList();
@@ -82,12 +90,17 @@ const changeToAdd = (type: string) => {
 }
 
 const updateSplit = () => {
+  const data: any = {
+    charCount: queryParam.value.charCount,
+    type: parseInt(queryParam.value.type),
+  }
+  if (curType.value === 'pdf') {
+    data.filePath = queryParam.value.filePath
+  } else {
+    data.content = queryParam.value.content
+  }
   baseService
-    .post("/knowledge/split", {
-      charCount: queryParam.value.charCount,
-      content: queryParam.value.content,
-      type: parseInt(queryParam.value.type),
-    })
+    .post("/knowledge/split", data)
     .then((res) => {
       if (res.code === 0) {
         splitList.value = res.data;
@@ -96,8 +109,13 @@ const updateSplit = () => {
 }
 
 const saveDoc = () => {
-  const data: any = queryParam.value;
+  const data: any = Object.assign({}, queryParam.value);
   data.type = parseInt(data.type);
+  if (curType.value === 'pdf') {
+    delete data.content;
+  } else {
+    delete data.filePath;
+  }
   saveLoading.value = true;
   baseService
     .post("/knowledge/knowledgeinfo/save", data)
@@ -126,7 +144,8 @@ const clearQueryParam = () => queryParam.value = {
   documentName: '',
   type: '1',
   content: '',
-  charCount: 100
+  charCount: 100,
+  filePath: ''
 }
 
 const gotoEdit = (index: number) => {
@@ -137,18 +156,54 @@ const gotoEdit = (index: number) => {
       content: list[index].content,
       documentName: list[index].documentName,
       type: list[index].type,
-      charCount: list[index].charCount
+      charCount: list[index].charCount,
+      filePath: list[index].filePath,
     }
 
     baseService
-    .get(`/knowledge/knowledgefragmentinfo/split/${list[index].knowledgeId}`)
-    .then((res) => {
-      if (res.code === 0) {
-      }
-    });
+      .get(`/knowledge/knowledgefragmentinfo/split/${list[index].knowledgeId}`)
+      .then((res) => {
+        if (res.code === 0) {
+        }
+      });
+  }
+}
+
+const hnadleChangeStatusTo2 = () => {
+  editStatus.value = 2;
+  updateSplit();
+}
+
+const beforeUpload: UploadProps['beforeUpload'] = (rawFile) => {
+  console.log(rawFile)
+  let types = [
+    'text/plain',
+    'application/pdf',
+    'application/msword',
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+  ];
+  if (!types.includes(rawFile.type) && rawFile.name.indexOf('.md') === -1) {
+    ElMessage.error('上传图片只能是 pdf，doc，docx，txt，md 格式!')
+    return false
+  }
+  if ('application/pdf' === rawFile.type && rawFile.size / 1024 / 1024 > 30) {
+    ElMessage.error('上传PDF文件大小不能超过 30MB!')
+    return false
   }
 
-  
+  if (rawFile.size / 1024 / 1024 > 10) {
+    ElMessage.error('上传文档大小不能超过 10MB!')
+    return false
+  }
+  return true
+
+}
+
+const handleAvatarSuccess: UploadProps['onSuccess'] = (res) => {
+  if (res.code === 0) {
+    queryParam.value.documentName = res.data.split('/').pop();
+    queryParam.value.filePath = res.data;
+  }
 }
 
 
@@ -230,10 +285,10 @@ const gotoEdit = (index: number) => {
       <el-button type="text" icon="Back" @click="editStatus = 0">返回文档</el-button>
     </div>
     <div>
-      <h2>添加{{ curType === 'file' ? '文件' : '在线文档' }}</h2>
+      <h2>添加{{ curType === 'pdf' ? '文件' : '在线文档' }}</h2>
     </div>
     <div>
-      <el-form label-position="top">
+      <el-form label-position="top" v-if="curType === 'txt'">
         <el-form-item label="分组">
           <el-select v-model="group" placeholder="分组" style="width: 50%" @change="getList">
             <el-option label="默认" value="0" />
@@ -246,9 +301,35 @@ const gotoEdit = (index: number) => {
           <el-input type="textarea" :rows="15" style="width: 100%;" v-model="queryParam.content"></el-input>
         </el-form-item>
       </el-form>
+      <el-form label-position="top" v-else>
+        <el-form-item label="分组">
+          <el-select v-model="group" placeholder="分组" style="width: 50%" @change="getList">
+            <el-option label="默认" value="0" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="文档标题">
+          <el-input disabled style="width: 100%;" v-model="queryParam.documentName" placeholder="文档标题"></el-input>
+        </el-form-item>
+        <el-form-item label="上传文档">
+          <el-upload accept=".pdf,.doc,.docx,.txt,.md" ref="uploadRef" style="width: 100%" drag class="upload-demo"
+            :before-upload="beforeUpload" :on-success="handleAvatarSuccess" :action="action" :headers="headers">
+            <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+            <div class="el-upload__text">
+              点击或拖拽文件到此区域上传
+            </div>
+            <template #tip>
+              <div class="el-upload__tip">
+                支持的格式：pdf，doc，docx，txt，md
+
+                大小限制：pdf不超过30MB，其它不超过10MB ｜ 单次最多上传200个文件
+              </div>
+            </template>
+          </el-upload>
+        </el-form-item>
+      </el-form>
     </div>
     <div class="bottom-btns" v-if="fillAddContent">
-      <el-button type="primary" @click="editStatus = 2">下一步</el-button>
+      <el-button type="primary" @click="hnadleChangeStatusTo2">下一步</el-button>
     </div>
   </div>
   <div class="container container-collection" v-else>
